94f7c08f60b486456008bbbe0a8d076678da0b64
Teodor K <133818762+greddy119@users.noreply.github.com>
2023-08-07 21:56:42 +0300

    am adaugat pagination, am adaugat in componentul ProductsPage

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+
+interface PaginationProps {
+  pageCount: number;
+  onPageChange: (selectedPage: number) => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({ pageCount, onPageChange }) => {
+  return (
+    <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+      <div className="flex flex-1 justify-between">
+        <ReactPaginate
+          pageCount={pageCount}
+          onPageChange={(selected) => onPageChange(selected.selected)}
+          containerClassName="pagination isolate inline-flex -space-x-px rounded-md shadow-sm"
+          pageClassName=" relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+          breakClassName=" relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+          previousLinkClassName="page-link relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+          nextLinkClassName="page-link relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+          disabledClassName="disabled"
+          activeClassName="active relative z-10 inline-flex items-center bg-blue-500 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
