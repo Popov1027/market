6f58928fe5693aff63f7c69c4522ed4bfda5e019
Grigore1 <popovicigrigore777@gmail.com>
2023-08-09 23:57:08 +0300

    modificari la code

--- src/components/Categories/ProductCategories.tsx
+++ src/components/Categories/ProductCategories.tsx
@@ -3,6 +3,12 @@ import axios from 'axios';
 import { Product } from '../ProductsPage/interface-response';
 import ProductCard from '../ProductsPage/ProductCard';
 
+const getProductsByCategory = async (category: string) =>
+  (await axios.get(`https://dummyjson.com/products/category/${category}`)).data.products;
+
+const getAllProducts = async () =>
+  (await axios.get('https://dummyjson.com/products')).data.products;
+
 const ProductsComponent = () => {
   const [categories, setCategories] = useState<string[]>([]);
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
@@ -10,77 +16,59 @@ const ProductsComponent = () => {
   const categoryContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    axios
-      .get('https://dummyjson.com/products/categories')
-      .then((response) => setCategories(response.data))
-      .catch((error) => console.log('Error', error));
+    axios.get('https://dummyjson.com/products/categories')
+        .then((response) => setCategories(response.data))
+        .catch((error) => console.log('Error', error));
   }, []);
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      if (selectedCategory) {
-        try {
-          const response = await axios.get(`https://dummyjson.com/products/category/${selectedCategory}`);
-          setProducts(response.data.products);
-        } catch (error) {
-          console.log('Error', error);
-        }
-      } else {
-        try {
-          const response = await axios.get('https://dummyjson.com/products');
-          setProducts(response.data.products);
-        } catch (error) {
-          console.log('Error', error);
-        }
-      }
-    };
-    fetchProducts();
+    (async () => {
+      const fetchedProducts = selectedCategory
+          ? await getProductsByCategory(selectedCategory).catch((error) =>
+              console.log('Error', error)
+          )
+          : await getAllProducts().catch((error) => console.log('Error', error));
+      setProducts(fetchedProducts || []);
+    })();
   }, [selectedCategory]);
 
-  const handleCategoryChange = (category: string) => {
-    setSelectedCategory(category);
-  };
+  const handleCategoryChange = (category: string) => setSelectedCategory(category);
+
+  const renderProductsSection = (header: string) => (
+      <div className="mt-10">
+        <h2 className="text-center mb-5">{header}</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+  );
 
   return (
-    <div>
-      <h2 className="text-left mb-5 text-2xl font-semibold">Product Categories:</h2>
-      <div ref={categoryContainerRef} className="flex overflow-x-auto gap-3 justify-start p-1 items-center">
-        {categories.map((category) => (
-          <button
-            key={category}
-            onClick={() => handleCategoryChange(category)}
-            className={`w-38 p-2 rounded-full flex justify-between
+      <div>
+        <h2 className="text-left mb-5 text-2xl font-semibold">Product Categories:</h2>
+        <div ref={categoryContainerRef} className="flex overflow-x-auto gap-3 justify-start p-1 items-center">
+          {categories.map((category) => (
+              <button
+                  key={category}
+                  onClick={() => handleCategoryChange(category)}
+                  className={`w-38 p-2 rounded-full flex justify-between
              ${selectedCategory === category ? 'bg-orange-500 text-white' : 'bg-gray-200 text-black'
-          } text-xs font-bold cursor-pointer uppercase transition-all duration-200 shadow-sm whitespace-nowrap`}
-          >
+                  } text-xs font-bold cursor-pointer uppercase transition-all duration-200 shadow-sm whitespace-nowrap`}
+              >
                 #{category}
-          </button>
-        ))}
+              </button>
+          ))}
+        </div>
+
+        {selectedCategory
+            ? renderProductsSection(`Products in ${selectedCategory} category:`)
+            : renderProductsSection('All Products:')
+        }
+
       </div>
-
-      {selectedCategory && (
-        <div className="mt-10">
-          <h2 className="text-center mb-5">Products in {selectedCategory} category:</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-      )}
-
-      {!selectedCategory && (
-        <div className="mt-10">
-          <h2 className="text-center mb-5">All Products:</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-      )}
-    </div>
   );
 };
-
 export default ProductsComponent;
+
