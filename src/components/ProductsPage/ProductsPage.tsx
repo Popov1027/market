d34bdefa6a101f43d80f30de6d430fb2340bb42a
Daniel20 <tatarud24@gmail.com>
2023-08-16 17:03:53 +0300

    user-verification

--- src/components/ProductsPage/ProductsPage.tsx
+++ src/components/ProductsPage/ProductsPage.tsx
@@ -2,25 +2,31 @@ import React, { useEffect, useState } from 'react';
 import { getProducts } from '../../service/ProductService/productServise';
 import { Product } from './interface-response';
 import ProductCard from './ProductCard';
-import ProductCategories from "../Categories/ProductCategories";
+import ProductCategories from '../Categories/ProductCategories';
+import { useNavigate } from 'react-router-dom';
 const ProductsPage: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
-
+  const navigate = useNavigate();
   useEffect(() => {
-    getProducts().then((data) => {
-      setProducts(data);
-    });
-  }, []);
+    const token = localStorage.getItem('token');
+    if (!token) {
+      navigate('/login');
+    } else {
+      getProducts().then((data) => {
+        setProducts(data);
+      });
+    }
+  }, [navigate]);
 
   return (
-      <div className="container mx-auto py-8">
-        <ProductCategories />
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
+    <div className="container mx-auto py-8">
+      <ProductCategories />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
+    </div>
   );
 };
 
--- src/service/CategoryService/categoryService.ts
+++ src/service/CategoryService/categoryService.ts
@@ -1,13 +1,13 @@
 import http from '../http';
 
 export const getCategories = () => {
-    return http.get('products/categories').then((response) => response.data);
+  return http.get('products/categories').then((response) => response.data);
 };
 
 export const getProductsByCategory = (category: string) => {
-    return http.get(`products/category/${category}`).then((response) => response.data.products);
+  return http.get(`products/category/${category}`).then((response) => response.data.products);
 };
 
 export const getAllProducts = () => {
-    return http.get('products').then((response) => response.data.products);
+  return http.get('products').then((response) => response.data.products);
 };
