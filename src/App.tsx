48a3271e67772ee2c1211ac1252042d35cba980c
Marius Sirbu <mariuss.sirbu@yandex.com>
2023-08-22 11:00:41 +0300

    nav bar with router render condition

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import ProductsPage from './components/ProductsPage/ProductsPage';
 import LoginUtils from './components/Login/LoginUtils';
 import { ProductSinglePage } from './components/ProductSinglePage/ProductSinglePage';
+import { NavBar } from './components/NavBar/NavBar';
 
 function App() {
+  const navBarDislay = location.pathname !== '/login';
   return (
     <Router>
+      {navBarDislay && <NavBar />}
       <Routes>
         <Route path="/login" element={<LoginUtils />} />
         <Route path="/" element={<ProductsPage />} />
